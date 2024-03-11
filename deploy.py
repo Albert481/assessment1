c62a3491037b5cad41fc4997fe56cb19ec7b0a13
Albert481 <albertyu481@gmail.com>
2024-03-12 01:04:31 +0800

    q2

---
+++ deploy.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    return {
+        "statusCode": 200,
+        "body": json.dumps(users)
+    }
+
